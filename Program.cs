78d17817b6f4ed5619fe08f63e7d0fc6149223fe
Michael Mason <mwmason1998@gmail.com>
2022-06-06 13:25:18 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿List<object> myList = new List<object>();
+myList.Add(7);
+myList.Add(28);
+myList.Add(-1);
+myList.Add(true);
+myList.Add("chair");
+
+int sum = 0;
+foreach(var item in myList){
+    Console.WriteLine(item);
+    if(item is int){
+        sum += (int) item;
+    }
+}
+Console.WriteLine(sum);
